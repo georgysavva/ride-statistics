32492439e3019097b0629c92ad230c774d0eea2b
Georgy <georgy.savva@gmail.com>
2021-01-12 22:22:30 +0800

    Add improve usage string of the binary. Rename the default input file

--- cmd/calculate-statistics/main.go
+++ cmd/calculate-statistics/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/georgysavva/ride-statistics/pkg/statistics"
 )
@@ -10,9 +12,23 @@ import (
 func main() {
 	var concurrency int
 	flag.IntVar(&concurrency, "concurrency", 64, "number of workers that will process file in parallel")
+	flag.Usage = func() {
+		fmt.Fprintf(
+			flag.CommandLine.Output(),
+			"Usage: %s [options] [input_file] [output_file]\n\n"+
+				"Positional arguments:\n"+
+				"1th argument, input_file - "+
+				"path to the input csv file with recorded rides (default recorded_rides.csv)\n"+
+				"2th argument, output_file - "+
+				"path to the output csv file to write statistics to (default statistics.csv)\n\n"+
+				"Options:\n",
+			os.Args[0],
+		)
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 
-	inputFile, outputFile := "paths.csv", "statistics.csv"
+	inputFile, outputFile := "recorded_rides.csv", "statistics.csv"
 	if flag.NArg() > 0 {
 		inputFile = flag.Arg(0)
 		if flag.NArg() > 1 {
